8f356b86c0d1af9d36078c58c8ddfac240882b07
jackhowerton <jackhowerton@gmail.com>
2020-02-13 22:59:28 -0800

    add start menu, connect scenes

--- src/CST.js
+++ src/CST.js
@@ -2,6 +2,7 @@ export const CST = {
     SCENES: {
         LOAD: "LOAD",
         MENU: "MENU",
+        OPTIONS: "OPTIONS",
         LEVEL1: "LEVEL1",
         LEVEL2: "LEVEL2",
         LEVEL3: "LEVEL3",
--- src/main.js
+++ src/main.js
@@ -1,16 +1,17 @@
 import { Load } from "./scenes/Load";
 import { Menu } from "./scenes/Menu";
+import { Options } from "./scenes/Options";
 import { Level1 } from "./scenes/Level1";
 import { Level2 } from "./scenes/Level2";
 import { Level3 } from "./scenes/Level3";
 import { Credits } from "./scenes/Credits";
 
 
-let game = new Phaser.Game({
+export const config = {
     width: 800,
     height: 600,
     scene: [
-        Load, Menu, Level1, Level2, Level3, Credits
+        Load, Menu, Options, Level1, Level2, Level3, Credits
     ],
     render: {
         pixelArt: true
@@ -25,4 +26,29 @@ let game = new Phaser.Game({
         mode: Phaser.Scale.FIT,
         autoCenter: Phaser.Scale.CENTER_BOTH
     }
-});
+};
+
+
+export const gameState = {
+    
+    // cursors: 7
+    
+    // active: true,
+  
+    // player: {x: 225, y: 470},
+    // platform: {},
+  
+    // bullets: {},
+    // bulletTime: 0,
+  
+    // enemies: 7,
+    // enemyVelocity: 1,
+  
+    // enemyBombs: {},
+  
+    // level: 0,
+    // lives: 3,
+    // score: 0,
+  }
+
+  let game = new Phaser.Game(config);
